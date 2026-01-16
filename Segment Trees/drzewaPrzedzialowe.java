62443bef79fba817572503f50a84ce6b05236b2d
Łukasz Matecki <lukasz.matecki12@gmail.com>
2026-01-17 00:06:00 +0100

    Refactor CannonTree class and update variable names

--- Segment Trees/drzewaPrzedzialowe.java
+++ Segment Trees/drzewaPrzedzialowe.java
@@ -1,91 +1,91 @@
 import java.util.*;
 
-class CannonTree
-{
+class CannonTree {
     private final int n;
-    private int rozmiar;
-    private final int[] suma;
-    private final long[] najpozniejszy_czas_zestrzelenia;
+    private int size;
+    private final int[] sum;
+    private final long[] last_hit_time;
     private static final long NEG = Long.MIN_VALUE / 4;
 
-    public CannonTree(int[] cele)
+    public CannonTree(int[] targets) 
     {
-        this.n = cele.length;
+        this.n = targets.length;
 
-        rozmiar = 1;
-        while (rozmiar < n) rozmiar <<= 1;
+        size = 1;
+        while (size < n) size <<= 1;
 
-        suma = new int[2 * rozmiar];
-        najpozniejszy_czas_zestrzelenia = new long[2 * rozmiar];
+        sum = new int[2 * size];
+        last_hit_time = new long[2 * size];
 
-        build(cele);
+        build(targets);
     }
 
-    private void build(int[] cele)
+    private void build(int[] targets) 
     {
-        for (int i = 0; i < rozmiar; i++)
+        for (int i = 0; i < size; i++) 
         {
-            int node = rozmiar + i;
+            int node = size + i;
 
-            if (i < n) {
-                int odwrocony_indeks = n - 1 - i;
-                int liczba_celow = cele[odwrocony_indeks];
+            if (i < n) 
+            {
+                int reversed_index = n - 1 - i;
+                int target_count = targets[reversed_index];
 
-                suma[node] = liczba_celow;
+                sum[node] = target_count;
 
-                if (liczba_celow > 0) { najpozniejszy_czas_zestrzelenia[node] = odwrocony_indeks + 2L * (liczba_celow - 1); }
-                else { najpozniejszy_czas_zestrzelenia[node] = NEG; }
-            }
-            else {
-                suma[node] = 0;
-                najpozniejszy_czas_zestrzelenia[node] = NEG;
+                if (target_count > 0) { last_hit_time[node] = reversed_index + 2L * (target_count - 1); } 
+                else { last_hit_time[node] = NEG; }
+            } else {
+                sum[node] = 0;
+                last_hit_time[node] = NEG;
             }
         }
-        for (int v = rozmiar - 1; v >= 1; v--) { pull(v);}
+
+        for (int v = size - 1; v >= 1; v--) {
+            pull(v);
+        }
     }
 
-    private void pull(int v)
+    private void pull(int v) 
     {
-        // v - indeks węzła rodzica
-        int L = 2 * v; // lewe dziecko
-        int R = 2 * v + 1; // prawe dziecko
+        int L = 2 * v;
+        int R = 2 * v + 1;
 
-        suma[v] = suma[L] + suma[R];
+        sum[v] = sum[L] + sum[R];
 
-        long przesuniecie_w_prawo = (najpozniejszy_czas_zestrzelenia[R] == NEG) ? NEG : najpozniejszy_czas_zestrzelenia[R] + 2L * suma[L];
-        najpozniejszy_czas_zestrzelenia[v] = Math.max(najpozniejszy_czas_zestrzelenia[L], przesuniecie_w_prawo);
+        long right_shift = (last_hit_time[R] == NEG) ? NEG : last_hit_time[R] + 2L * sum[L];
+        last_hit_time[v] = Math.max(last_hit_time[L], right_shift);
     }
 
-    public void update(int indeks, int nowa_wartosc) {
-        int odwrotny_indeks = n - 1 - indeks;
-        int pos = rozmiar + odwrotny_indeks;
+    public void update(int index, int new_value) 
+    {
+        int reversed_index = n - 1 - index;
+        int pos = size + reversed_index;
 
-        suma[pos] = nowa_wartosc;
+        sum[pos] = new_value;
 
-        if (nowa_wartosc > 0) { najpozniejszy_czas_zestrzelenia[pos] = indeks + 2L * (nowa_wartosc - 1); }
-        else { najpozniejszy_czas_zestrzelenia[pos] = NEG; }
+        if (new_value > 0) { last_hit_time[pos] = index + 2L * (new_value - 1); } 
+        else { last_hit_time[pos] = NEG; }
 
         pos /= 2;
-        while (pos >= 1)
-        {
+        while (pos >= 1) {
             pull(pos);
             pos /= 2;
         }
     }
-    public long getLastHitTime() { return najpozniejszy_czas_zestrzelenia[1] == NEG ? 0 : najpozniejszy_czas_zestrzelenia[1]; }
+
+    public long getLastHitTime() { return last_hit_time[1] == NEG ? 0 : last_hit_time[1]; }
 }
 
-public class drzewaPrzedzialowe
-{
-    public static void main(String[] args)
-    {
+public class SegmentTreesDemo {
+    public static void main(String[] args) {
         int[] targets = {0, 2, 0, 0, 3};
         CannonTree cannon = new CannonTree(targets);
 
-        System.out.println("Czas zestrzelenia wszystkich celów: " + cannon.getLastHitTime());
+        System.out.println("Time to hit all targets: " + cannon.getLastHitTime());
         cannon.update(1, 1);
-        System.out.println("Po zmianie odległości 1 na 1: " + cannon.getLastHitTime());
+        System.out.println("After updating index 1 to 1: " + cannon.getLastHitTime());
         cannon.update(2, 3);
-        System.out.println("Po zmianie odległości 2 na 3: " + cannon.getLastHitTime());
+        System.out.println("After updating index 2 to 3: " + cannon.getLastHitTime());
     }
 }
